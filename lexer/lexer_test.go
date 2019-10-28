75509050eb197f264e67bc27e3f1aee7eb7a1faa
Andrej Rokavec <andrej.rokavec@gmail.com>
2019-10-28 18:10:51 +0100

    Refactor lexer tests

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -9,93 +9,171 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `
-fn Add(a i32, b i32) : i32 {
-	return a + b
-}~
-2 - 1;
-a := 4 + 5
-a = a + 2
-f := 0.6
-import fn log(num i32)
-if a != f {}
-name := "shift"
-`
+	t.Parallel()
 
-	tests := []struct {
+	type output struct {
 		tokenType token.Type
 		literal   string
-	}{
-		{tokenType: token.FUNC, literal: "fn"},
-		{tokenType: token.IDENT, literal: "Add"},
-		{tokenType: token.LPAREN, literal: "("},
-		{tokenType: token.IDENT, literal: "a"},
-		{tokenType: token.IDENT, literal: "i32"},
-		{tokenType: token.COMMA, literal: ","},
-		{tokenType: token.IDENT, literal: "b"},
-		{tokenType: token.IDENT, literal: "i32"},
-		{tokenType: token.RPAREN, literal: ")"},
-		{tokenType: token.COLON, literal: ":"},
-		{tokenType: token.IDENT, literal: "i32"},
-		{tokenType: token.LCURLY, literal: "{"},
-		{tokenType: token.RETURN, literal: "return"},
-		{tokenType: token.IDENT, literal: "a"},
-		{tokenType: token.PLUS, literal: "+"},
-		{tokenType: token.IDENT, literal: "b"},
-		{tokenType: token.RCURLY, literal: "}"},
-		{tokenType: token.ILLEGAL, literal: "~"},
-		{tokenType: token.INT, literal: "2"},
-		{tokenType: token.MINUS, literal: "-"},
-		{tokenType: token.INT, literal: "1"},
-		{tokenType: token.SEMICOLON, literal: ";"},
-		{tokenType: token.IDENT, literal: "a"},
-		{tokenType: token.INIT_ASSIGN, literal: ":="},
-		{tokenType: token.INT, literal: "4"},
-		{tokenType: token.PLUS, literal: "+"},
-		{tokenType: token.INT, literal: "5"},
-		{tokenType: token.IDENT, literal: "a"},
-		{tokenType: token.ASSIGN, literal: "="},
-		{tokenType: token.IDENT, literal: "a"},
-		{tokenType: token.PLUS, literal: "+"},
-		{tokenType: token.INT, literal: "2"},
-		{tokenType: token.IDENT, literal: "f"},
-		{tokenType: token.INIT_ASSIGN, literal: ":="},
-		{tokenType: token.FLOAT, literal: "0.6"},
-		{tokenType: token.IMPORT, literal: "import"},
-		{tokenType: token.FUNC, literal: "fn"},
-		{tokenType: token.IDENT, literal: "log"},
-		{tokenType: token.LPAREN, literal: "("},
-		{tokenType: token.IDENT, literal: "num"},
-		{tokenType: token.IDENT, literal: "i32"},
-		{tokenType: token.RPAREN, literal: ")"},
-		{tokenType: token.IF, literal: "if"},
-		{tokenType: token.IDENT, literal: "a"},
-		{tokenType: token.NOT_EQ, literal: "!="},
-		{tokenType: token.IDENT, literal: "f"},
-		{tokenType: token.LCURLY, literal: "{"},
-		{tokenType: token.RCURLY, literal: "}"},
-		{tokenType: token.IDENT, literal: "name"},
-		{tokenType: token.INIT_ASSIGN, literal: ":="},
-		{tokenType: token.STRING, literal: "shift"},
-		{tokenType: token.EOF, literal: string(rune(token.EOF))},
 	}
 
-	lex := lexer.New(strings.NewReader(input))
+	testCases := []struct {
+		name    string
+		input   string
+		outputs []output
+	}{
+		{
+			name: "function",
+			input: `fn Add(a i32, b i32) : i32 {
+				return a + b
+			}`,
+			outputs: []output{
+				{tokenType: token.FUNC, literal: "fn"},
+				{tokenType: token.IDENT, literal: "Add"},
+				{tokenType: token.LPAREN, literal: "("},
+				{tokenType: token.IDENT, literal: "a"},
+				{tokenType: token.IDENT, literal: "i32"},
+				{tokenType: token.COMMA, literal: ","},
+				{tokenType: token.IDENT, literal: "b"},
+				{tokenType: token.IDENT, literal: "i32"},
+				{tokenType: token.RPAREN, literal: ")"},
+				{tokenType: token.COLON, literal: ":"},
+				{tokenType: token.IDENT, literal: "i32"},
+				{tokenType: token.LCURLY, literal: "{"},
+				{tokenType: token.RETURN, literal: "return"},
+				{tokenType: token.IDENT, literal: "a"},
+				{tokenType: token.PLUS, literal: "+"},
+				{tokenType: token.IDENT, literal: "b"},
+				{tokenType: token.RCURLY, literal: "}"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "illegal symbol",
+			input: `~`,
+			outputs: []output{
+				{tokenType: token.ILLEGAL, literal: "~"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "subtraction expression with semicolon",
+			input: `2 - 1;`,
+			outputs: []output{
+				{tokenType: token.INT, literal: "2"},
+				{tokenType: token.MINUS, literal: "-"},
+				{tokenType: token.INT, literal: "1"},
+				{tokenType: token.SEMICOLON, literal: ";"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "assignment with initialization with addition expression",
+			input: `a := 4 + 5`,
+			outputs: []output{
+				{tokenType: token.IDENT, literal: "a"},
+				{tokenType: token.INIT_ASSIGN, literal: ":="},
+				{tokenType: token.INT, literal: "4"},
+				{tokenType: token.PLUS, literal: "+"},
+				{tokenType: token.INT, literal: "5"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "assignment with addition expression",
+			input: `a = a + 2`,
+			outputs: []output{
+				{tokenType: token.IDENT, literal: "a"},
+				{tokenType: token.ASSIGN, literal: "="},
+				{tokenType: token.IDENT, literal: "a"},
+				{tokenType: token.PLUS, literal: "+"},
+				{tokenType: token.INT, literal: "2"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "assignment with initialization floating point number",
+			input: `f := 0.6`,
+			outputs: []output{
+				{tokenType: token.IDENT, literal: "f"},
+				{tokenType: token.INIT_ASSIGN, literal: ":="},
+				{tokenType: token.FLOAT, literal: "0.6"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "import external function",
+			input: `import fn log(num i32)`,
+			outputs: []output{
+				{tokenType: token.IMPORT, literal: "import"},
+				{tokenType: token.FUNC, literal: "fn"},
+				{tokenType: token.IDENT, literal: "log"},
+				{tokenType: token.LPAREN, literal: "("},
+				{tokenType: token.IDENT, literal: "num"},
+				{tokenType: token.IDENT, literal: "i32"},
+				{tokenType: token.RPAREN, literal: ")"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "if expression",
+			input: `if a != f {}`,
+			outputs: []output{
+				{tokenType: token.IF, literal: "if"},
+				{tokenType: token.IDENT, literal: "a"},
+				{tokenType: token.NOT_EQ, literal: "!="},
+				{tokenType: token.IDENT, literal: "f"},
+				{tokenType: token.LCURLY, literal: "{"},
+				{tokenType: token.RCURLY, literal: "}"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "string assignment",
+			input: `name := "shift"`,
+			outputs: []output{
+				{tokenType: token.IDENT, literal: "name"},
+				{tokenType: token.INIT_ASSIGN, literal: ":="},
+				{tokenType: token.STRING, literal: "shift"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+		{
+			name:  "string assignment with interpolated variable",
+			input: `interpolate := "this $a variable"`,
+			outputs: []output{
+				{tokenType: token.IDENT, literal: "interpolate"},
+				{tokenType: token.INIT_ASSIGN, literal: ":="},
+				{tokenType: token.STRING, literal: "this $a variable"},
+				{tokenType: token.EOF, literal: string(rune(token.EOF))},
+			},
+		},
+	}
 
-	for _, test := range tests {
-		tok := lex.NextToken()
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-		if tok.Type != test.tokenType {
-			t.Errorf("line %d, column %d - wrong type. Expected %q but got %q", tok.Pos.Line, tok.Pos.Column, token.Print(test.tokenType), token.Print(tok.Type))
-		}
+			lex := lexer.New(strings.NewReader(tc.input))
 
-		if tok.Lit != test.literal {
-			t.Errorf("line %d, column %d - wrong literal. Expected %q but got %q", tok.Pos.Line, tok.Pos.Column, test.literal, tok.Lit)
-		}
+			for _, output := range tc.outputs {
+				tok := lex.NextToken()
+
+				if tok.Type != output.tokenType {
+					t.Errorf("line %d, column %d - wrong type. Expected %q but got %q", tok.Pos.Line, tok.Pos.Column, token.Print(output.tokenType), token.Print(tok.Type))
+				}
+
+				if tok.Lit != output.literal {
+					t.Errorf("line %d, column %d - wrong literal. Expected %q but got %q", tok.Pos.Line, tok.Pos.Column, output.literal, tok.Lit)
+				}
+			}
+		})
 	}
 }
 
 func TestTokenPosition(t *testing.T) {
+	t.Parallel()
+
 	input := `
 fn Sub(a i32, b i32) : i32 {
 	return a - b
